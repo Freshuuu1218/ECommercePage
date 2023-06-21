9214765007578d03f43513235909d6baba87aada
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-06-21 16:46:05 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+import { ImagesComponent } from './images/images.component';
+import { DescriptionComponent } from './description/description.component';
+import { CountComponent } from './count/count.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    MobileMenuComponent,
+    ImagesComponent,
+    DescriptionComponent,
+    CountComponent
   ],
   imports: [
     BrowserModule,
